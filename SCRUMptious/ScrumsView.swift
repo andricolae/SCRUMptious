1029dc6b929fef3d2b3905a701efeef219bba38f
andricolae <andreicalutiu@gmail.com>
2023-05-06 00:12:42 +0300

    added functionality to the demo app from Apple

---
+++ SCRUMptious/ScrumsView.swift
@@ -0,0 +1,34 @@
+//
+//  ScrumsView.swift
+//  SCRUMptious
+//
+//  Created by Andrei-Nicolae Calutiu on 06.05.2023.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }                       .listRowBackground(scrum.theme.mainColor)
+            }
+                .navigationTitle("Daily Scrums")
+                .toolbar {
+                    Button(action: {}) {
+                        Image(systemName: "plus")
+                    }
+                        .accessibilityLabel("New Scrum")
+                }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
