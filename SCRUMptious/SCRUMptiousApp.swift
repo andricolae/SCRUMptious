723b1dccb83952ad2a54749b6df9326b80829c4d
andricolae <andreicalutiu@gmail.com>
2023-05-05 14:10:11 +0300

    Initial Commit

---
+++ SCRUMptious/SCRUMptiousApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCRUMptiousApp.swift
+//  SCRUMptious
+//
+//  Created by Andrei-Nicolae Calutiu on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SCRUMptiousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
